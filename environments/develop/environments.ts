ad399b4154a674d440695dadadbdd7a3661f436e
chopyk89 <if040489cvv@gmail.com>
2025-01-24 22:44:44 +0200

    init

---
+++ environments/develop/environments.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyD_N9ZYXvOzQs16AZ-I4-lOML74hNgtQqA",
+    authDomain: "weather-6844b.firebaseapp.com",
+    projectId: "weather-6844b",
+    storageBucket: "weather-6844b.firebasestorage.app",
+    messagingSenderId: "472606239919",
+    appId: "1:472606239919:web:cab8a022c2721b1ba8785a",
+    measurementId: "G-F24FFWQLVW"
+  },
+  production: false,
+};
